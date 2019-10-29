a2c55876e25641315bc89e1e4fda532ff24ea156
Victor Cherezov <victor_cherezov@mentor.com>
2019-10-29 18:04:46 +0300

    excel win

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from app import app
 from app.forms import FindForm
 from app import utils
 
-
+setups = list()
 
 @app.route('/')
 @app.route('/index')
@@ -22,12 +22,19 @@ def index():
 @app.route('/find_builds', methods=['GET', 'POST'])
 def find_builds():
     form = FindForm()
+    global setups
     if form.validate_on_submit():
         setups = utils.find_builds(form.build.data, form.tag.data)
         setups_count = len(setups)
 
         return render_template('result.html', title='result', form=form, setups=setups, setups_count=setups_count)
         # flash("Find build {}, tag {}".format(form.build.data, form.tag.data))
+    setups = list()
     return render_template('find_builds.html', title='Find', form=form)
 
 
+@app.route('/make_xls')
+def make_xls():
+    res = utils.make_xls(setups)
+    count = len(res)
+    return render_template('make_xls.html', title='xls', res=res, count=count )
--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,8 @@ import json
 import os
 from pprint import pprint
 import re
-
 from win32com import client
+import pythoncom
 
 
 def find_builds(build, tag):
@@ -22,38 +22,44 @@ def find_builds(build, tag):
     return setups
 
 
-# result = list()
-# with open('snap_dct.json') as fi:
-#     vms = json.load(fi)
-#
-# for _setup in setups:
-#     setup_prefix = os.path.basename(_setup).split('-')[0]
-#     snapshot_prefix = cfg_dct['prod_snaps'][setup_prefix]
-#     for _vm in vms:
-#         vm_name = _vm.replace('.vmx', '')
-#         vm_path = vms[_vm]['pth']
-#         for k in vms[_vm]['sn']:
-#             if k.startswith(snapshot_prefix):
-#                 result.append((_setup,  vm_name, vm_path,  k, "0"))
-#
-# # pprint(result)
-#
-# job_file = r'c:\experiments\work_py\VM-Monitor.Jobs.xls'
-# xls = client.Dispatch("Excel.Application")
-# wb = xls.Workbooks.Add()
-# sheet = wb.WorkSheets("Sheet1")
-# sheet.Name = "Table"
+def make_xls(setups):
+    result = list()
+    with open(r'c:\experiments\test_flask\app\cfg.json') as fi:
+        cfg_dct = json.load(fi)
 
-# header
-# header_list = ["InstallPath", "Name", "Path", "SnapName", "Done"]
-# for i in range(len(header_list)):
-#     sheet.Cells(1, i + 1).value = header_list[i]
-#
-# for i in range(len(result)):
-#     for j in range(5):
-#         # print(result[i][j])
-#         sheet.Cells(i + 2, j + 1).value = result[i][j]
-#
-# wb.SaveAs(job_file, 56)
-# wb.Close()
+    with open(r'c:\experiments\test_flask\app\snap_dct.json') as fi:
+        vms = json.load(fi)
+
+    for _setup in setups:
+        setup_prefix = os.path.basename(_setup).split('-')[0]
+        snapshot_prefix = cfg_dct['prod_snaps'][setup_prefix]
+        for _vm in vms:
+            vm_name = _vm.replace('.vmx', '')
+            vm_path = vms[_vm]['pth']
+            for k in vms[_vm]['sn']:
+                if k.startswith(snapshot_prefix):
+                    result.append((_setup,  vm_name, vm_path,  k, "0"))
+
+    job_file = r'c:\experiments\test_flask\VM-Monitor.Jobs.xls'
+    pythoncom.CoInitialize()
+    xls = client.Dispatch("Excel.Application")
+
+    wb = xls.Workbooks.Add()
+    sheet = wb.WorkSheets("Sheet1")
+    sheet.Name = "Table"
+
+    # header
+    header_list = ["InstallPath", "Name", "Path", "SnapName", "Done"]
+    for i in range(len(header_list)):
+        sheet.Cells(1, i + 1).value = header_list[i]
+
+    for i in range(len(result)):
+        for j in range(5):
+            sheet.Cells(i + 2, j + 1).value = result[i][j]
+
+    wb.SaveAs(job_file, 56)
+    wb.Close()
+    pythoncom.CoUninitialize()
+
+    return result
 
