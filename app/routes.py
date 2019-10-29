6f3fecc9d654840378ebf9705676b06ad47d9a1f
Victor Cherezov <victor_cherezov@mentor.com>
2019-10-29 15:03:00 +0300

    test branch

--- app/routes.py
+++ app/routes.py
@@ -25,14 +25,18 @@ def index():
 def find_builds():
     form = FindForm()
     if form.validate_on_submit():
-        flash("Find build {}, tag {}".format(form.build.data, form.tag.data))
-        return redirect(url_for('result'))
+        setups = ["111111111111", "22222222222222", "3333333333333", "44444444444444"]
+        setups_count = len(setups)
+
+        return render_template('result.html', title='result', form=form, setups=setups, setups_count=setups_count)
+        # flash("Find build {}, tag {}".format(form.build.data, form.tag.data))
+        # return redirect(url_for('result'))
     return render_template('find_builds.html', title='Find', form=form)
 
 
-@app.route('/result')
-def result():
-    setups = ["111111111111", "22222222222222", "3333333333333", "44444444444444"]
-    setups_count = len(setups)
-
-    return render_template('result.html', title='Result', setups=setups, setups_count=setups_count)
+# @app.route('/result')
+# def result():
+#     setups = ["111111111111", "22222222222222", "3333333333333", "44444444444444"]
+#     setups_count = len(setups)
+#
+#     return render_template('result.html', title='Result', setups=setups, setups_count=setups_count)
